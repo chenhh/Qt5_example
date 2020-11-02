6719740361d88395219b50b9afd284d1eeb074fd
chenhh_x220 <chen1116@gmail.com>
2020-11-02 15:30:11 +0800

    update work thread example

--- QThread_movetothread_example/worker.cpp
+++ QThread_movetothread_example/worker.cpp
@@ -8,6 +8,7 @@ Worker::Worker(QObject *parent) : QObject(parent)
 
 void Worker::on_doSomething()
 {
-  qDebug() << "I'm working in thread:" << QThread::currentThreadId();
-  emit resultReady("Hello");
+    qDebug() << "I'm working in thread:" << QThread::currentThreadId();
+    /* 工作結束後，發送訊*/
+    emit resultReady("Hello");
 }
