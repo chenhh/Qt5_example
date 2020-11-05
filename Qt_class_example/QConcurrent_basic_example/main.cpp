cf1cf05000977446dd9d8f221584df7715a3a0f6
chenhh_x220 <chen1116@gmail.com>
2020-11-05 10:02:51 +0800

    add resource example

--- Qt_class_example/QConcurrent_basic_example/main.cpp
+++ Qt_class_example/QConcurrent_basic_example/main.cpp
@@ -27,6 +27,7 @@ void map_example()
     /* (10, 20, 30, 40, 50)
      * 直接修改list的內容
      */
+    qDebug()<<"map example";
     qDebug() << list;
 }
 
@@ -40,6 +41,7 @@ void mapped_example()
      * 回傳複製修改後的內容
      */
     future.waitForFinished();
+    qDebug()<<"mapped example";
     qDebug() <<list;
     qDebug() << future.results();
 }
@@ -49,18 +51,57 @@ void map_reduced_example()
     QList<int> list {1,2,3,4,5};
     QFuture<int> future = QtConcurrent::mappedReduced(list, function2, reducedFunction);
     future.waitForFinished();
+    qDebug()<<"map_reduce example";
     qDebug() << future.result(); //150
+}
+
+
+bool filter_function(const int &num)
+{
+    return num > 3;
+}
+
+void filter_example()
+{
+    QList<int> list {1,2,3,4,5};
+    QFuture<void> future  = QtConcurrent::filter(list, filter_function);
+    future.waitForFinished();
+    /* (4, 5)
+     * 直接修改list的內容, 保留條件為true的元素
+     */
+    qDebug()<<"filter example";
+    qDebug() << list;
+}
+
+bool filtered_function(const int &num)
+{
+    return num > 3;
+}
+
+
+void filtered_example()
+{
+    QList<int> list {1,2,3,4,5};
+    auto future  = QtConcurrent::filtered(list, filtered_function);
+    future.waitForFinished();
+    /* (1,2,3, 4, 5)
+     * (4, 5)
+     * 複制list的內容, 保留條件為true的元素
+     */
+    qDebug()<<"filtered example";
+    qDebug() << list;
+    qDebug() << future.results();
 }
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug()<<"map example";
+
     map_example();
-    qDebug()<<"mapped example";
     mapped_example();
-    qDebug()<<"map_reduce example";
     map_reduced_example();
+    filter_example();
+    filtered_example();
     return a.exec();
 }
 
